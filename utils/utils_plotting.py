4733baa665fd095b508ff65691d030fc37a3210b
Sunand Raghupathi <sunraghupathi@gmail.com>
2020-09-11 11:37:29 -0400

    cam and points 3d plotting function

--- utils/utils_plotting.py
+++ utils/utils_plotting.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import plotly.graph_objs as go
 import cv2
+from scipy.spatial.transform import Rotation as R
 
 
 # this function assumes that we're plotting the first n frames of our dictionaries
@@ -396,4 +397,50 @@ def skew(vector):
             [-vector[1], vector[0], 0],
         ]
     )
+
+
+def plot_cams_and_points(cam_group=None, points_3d=None, title=None):
+    """
+    Plots the coordinate systems of the cameras along with the 3D points
+    """
+    if points_3d is not None:
+        scatter_data = [
+            go.Scatter3d(
+                x=points_3d[:, 0],
+                y=points_3d[:, 1],
+                z=points_3d[:, 2],
+                mode="markers",
+                marker=dict(size=2),
+            )
+        ]
+
+        data = scatter_data
+    else:
+        data = []
+
+    if cam_group is not None:
+        for i, cam in enumerate(cam_group.cameras):
+            rot_vec = R.from_rotvec(cam.init_rot)
+            R_mat = rot_vec.as_matrix()
+            t = cam.init_trans
+
+            data += vector_plot(
+                [R_mat[:, 0], R_mat[:, 1], R_mat[:, 2]], orig=t, cam_name=f"cam_{i+1}"
+            )
+
+    layout = go.Layout(margin=dict(l=4, r=4, b=4, t=4))
+
+    fig = go.Figure(data=data, layout=layout)
+    fig.update_traces(textfont_size=6)
+    fig.update_layout(
+        title={
+            "text": title,
+            "y": 0.9,
+            "x": 0.5,
+            "xanchor": "center",
+            "yanchor": "top",
+        }
+    )
+
+    fig.show()
 
